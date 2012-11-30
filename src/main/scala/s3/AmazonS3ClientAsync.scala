d7881b0564f040cc274dba2f6a963fb72e15a2f9
Zach Cox <zcox522@gmail.com>
2012-11-30 12:39:36 -0600

    Convenience methods

--- src/main/scala/s3/AmazonS3ClientAsync.scala
+++ src/main/scala/s3/AmazonS3ClientAsync.scala
@@ -3,7 +3,7 @@ package com.pongr.bangarang.s3
 import com.amazonaws.services.s3._
 import com.amazonaws.services.s3.model._
 import akka.dispatch._
-import java.io.{File, InputStream}
+import java.io.{File, InputStream, ByteArrayInputStream}
 
 class AmazonS3ClientAsync(s3: AmazonS3Client, implicit val context: ExecutionContext) {
   def putObjectAsync(request: PutObjectRequest): Future[PutObjectResult] = 
@@ -14,6 +14,16 @@ class AmazonS3ClientAsync(s3: AmazonS3Client, implicit val context: ExecutionCon
 
   def putObjectAsync(bucket: String, key: String, input: InputStream, metadata: ObjectMetadata): Future[PutObjectResult] = 
     Future { s3.putObject(bucket, key, input, metadata) }
+
+  def putBytesAsync(bucket: String, key: String, bytes: Array[Byte], contentType: String): Future[PutObjectResult] = {
+    val metadata = new ObjectMetadata() //TODO would be nice to have a case class version of ObjectMetadata, along with implicit conversions
+    metadata.setContentType("image/jpeg")
+    metadata.setContentLength(bytes.size)
+    putObjectAsync(bucket, key, new ByteArrayInputStream(bytes), metadata)
+  }
+
+  def putJpegAsync(bucket: String, key: String, bytes: Array[Byte]): Future[PutObjectResult] = 
+    putBytesAsync(bucket, key, bytes, "image/jpeg")
 }
 
 object AmazonS3ClientAsync {
